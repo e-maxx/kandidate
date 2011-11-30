0881a24d7270b7b7bc8b0ea5a2fee0bce786d5ea
e-maxx <ivanov@drucom.ru>
2011-12-01 00:06:08 +0300

    Добавлен класс "quaternion".

---
+++ types/quaternion.hpp
@@ -0,0 +1,136 @@
+/*! \file quaternion.hpp
+    \brief Содержит класс "кватернион".
+*/
+
+#pragma once
+#ifndef TYPES_QUATERNION_H
+#define TYPES_QUATERNION_H
+
+
+
+#include <cmath>
+#include <stdexcept>
+#include "vector3.hpp"
+
+
+
+/*! Класс "Кватернион".
+ *
+ * Кватернион - это гиперкомплексное число с четырьмя компонентами (w, x, y, z).
+ */
+class quaternion {
+
+public:
+
+	
+	/*! Компоненты кватерниона. */
+	//@{
+	double w, x, y, z;
+	//@}
+
+	
+	//! Конструктор нулевого кватерниона.
+	quaternion()
+		: w(0), x(0), y(0), z(0)
+	{ }
+
+	quaternion (double w, double x, double y, double z)
+		: w(w), x(x), y(y), z(z)
+	{ }
+
+	//! Конструктор кватерниона от скалярной и векторной частей.
+	quaternion (double w, const vector3 & v)
+		: w(w), x(v.x), y(v.y), z(v.z)
+	{ }
+
+	//! Конструктор кватерниона с нулевой векторной частью.
+	explicit quaternion (double num)
+		: w(num), x(0), y(0), z(0)
+	{ }
+
+	//! Конструктор кватерниона с нулевой скалярной частью.
+	quaternion (const vector3 & v)
+		: w(0), x(v.x), y(v.y), z(v.z)
+	{ }
+
+
+	/*! Индексированный доступ к компонентам кватерниона.
+	 *
+	 * @throws std::invalid_argument В случае, если передано некорректное значение idx.
+	 */
+	double operator[] (int idx) {
+		if (idx == 0)  return w;
+		if (idx == 1)  return x;
+		if (idx == 2)  return y;
+		if (idx == 3)  return z;
+		throw std::invalid_argument ("Invalid idx value.");
+	}
+
+	
+	quaternion operator+ (const quaternion & q) const {
+		return quaternion (w+q.w, x+q.x, y+q.y, z+q.z);
+	}
+
+	quaternion operator- (const quaternion & q) const {
+		return quaternion (w-q.w, x-q.x, y-q.y, z-q.z);
+	}
+
+	//! Умножение на константу.
+	quaternion operator* (double num) const {
+		return quaternion (w*num, x*num, y*num, z*num);
+	}
+
+	//! Кватернионное произведение.
+	quaternion operator* (const quaternion & q) const {
+		quaternion result;
+		result.w = w * q.w - x * q.x - y * q.y - z * q.z;
+		result.x = w * q.x + x * q.w + y * q.z - z * q.y;
+		result.y = w * q.y - x * q.z + y * q.w + z * q.x;
+		result.z = w * q.z + x * q.y - y * q.x + z * q.w;
+		return result;
+	}
+
+	quaternion operator- () const {
+		return quaternion (-w, -x, -y, -z);
+	}
+
+	//! Сопряжённый кватернион.
+	quaternion conjugate() const {
+		return quaternion (w, -x, -y, -z);
+	}
+
+	//! Обратный кватернион - умножение на который слева/справа эквивалентно делению слева/справа на исходный кватернион.
+	quaternion inverse() const {
+		return conjugate() * (1 / norm());
+	}
+
+
+	//! Возвращает норму кватерниона - сумму квадратов компонент.
+	double norm() const {
+		return w*w + x*x + y*y + z*z;
+	}
+
+	//! Возвращает длину (тензор) кватерниона - квадратный корень из суммы квадратов компонент.
+	double length() const {
+		return sqrt (norm());
+	}
+
+
+	//! Возвращает единичный кватернион.
+	static quaternion get_unit() {
+		return quaternion (1);
+	}
+
+
+}; // class quaternion
+
+
+
+//! Возвращает расстояние между кватернионами - т.е. модуль их разности.
+inline double distance (const quaternion & a, const quaternion & b) {
+	return (a-b).length();
+}
+
+
+
+#endif // ifndef TYPES_QUATERNION_H
