ca1f35c14a24439f052611f1dcc00b509f83b59a
e-maxx <ivanov@drucom.ru>
2011-12-01 01:38:57 +0300

    Добавил файл с константами (пока только константа "EPS").

---
+++ constants.hpp
@@ -0,0 +1,14 @@
+/*! \file constants.hpp
+    \brief Содержит общие константы.
+*/
+
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+//! Константа, с которой сравниваются дробные числа.
+#define EPS 1E-9
+
+
+#endif // CONSTANTS_H
