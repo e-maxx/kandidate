7c2bdb406b3ff22ff39b20190f886dedf0a45af7
e-maxx <ivanov@drucom.ru>
2011-12-07 00:25:12 +0300

    Внёс автоматически сгенерированный двухшаговый алгоритм.

---
+++ algorithms/auto_generated/2_step.hpp
@@ -0,0 +1,66 @@
+/** \file 2_step.hpp
+    \brief Содержит алгоритм "auto_generated_2_step_algorithm" - автоматически сгенерированный двухшаговый алгоритм.
+*/
+
+#pragma once
+#ifndef ALGORITHMS_AUTO_GENERATED_2_STEP_H
+#define ALGORITHMS_AUTO_GENERATED_2_STEP_H
+
+
+
+#include <cmath>
+#include "../algorithm.hpp"
+
+
+
+/** Класс "auto_generated_2_step_algorithm" - автоматически сгенерированный двухшаговый алгоритм.
+ */
+template <typename Q, typename I>
+class auto_generated_2_step_algorithm : public algorithm<Q,I> {
+
+public:
+
+
+	//! Запускает алгоритм, возвращая полученные результаты работы.
+	virtual t_output_data_ptr execute() {
+		t_output_data_ptr result = init_output_data_();
+
+		for (size_t i=1; i<result->get_count(); ++i) {
+			I phi = input_data_->get_integrated (result->ts[i-1], result->ts[i]);
+			quaternion lambda;
+
+			if (i == 1) {
+				double phi_m = phi.length();
+
+				lambda = quaternion (
+					cos (phi_m / 2),
+					phi * sin (phi_m / 2) / phi_m
+				);
+			}
+			else {
+				I phi2 = input_data_->get_integrated (result->ts[i-2], result->ts[i-1]);
+				
+				Q omega1 = phi;
+				Q omega2 = phi2;
+
+				lambda = quaternion(1) + 0.5 * omega1 + 17.0/192 * omega1*omega1 + 1./64 * omega1*omega2 + 1./64 * omega2*omega1 + 1./192 * omega2*omega2;
+			}
+
+			(*result)[i] = (*result)[i-1] * lambda;
+		}
+
+		return result;
+	}
+
+
+	//! Возвращает название алгоритма в виде строки.
+	virtual std::string get_algorithm_title() {
+		return "Автоматически сгенерированный алгоритм (2-шаговый)";
+	}
+
+
+}; // class auto_generated_2_step_algorithm
+
+
+
+#endif // ifndef ALGORITHMS_AUTO_GENERATED_2_STEP_H
