e19ab3691a2d83c7bfe2f8425c9fe0eac14de23d
e-maxx <ivanov@drucom.ru>
2011-12-08 14:41:06 +0300

    Добавлены конструкторы и методы для связи класса biquaternion и классов dual_number, dual_vector.

--- types/biquaternion.hpp
+++ types/biquaternion.hpp
@@ -9,13 +9,15 @@
 
 
 #include <cmath>
+#include "dual_number.hpp"
+#include "dual_vector.hpp"
 #include "quaternion.hpp"
 
 
 
 /** Класс "Бикватернион".
  *
- * Бикватернион - это число вида a + s b, где a и b - кватернионы, s - мнимая единица (такая, что s^2 = 0).
+ * Бикватернион - это дуальный кватернион вида a + s b, где a и b - кватернионы, s - мнимая единица (такая, что s^2 = 0).
  */
 class biquaternion {
 
@@ -33,11 +35,37 @@ public:
 		: a(), b()
 	{ }
 
-	biquaternion (quaternion a, quaternion b)
+	biquaternion (const quaternion & a, const quaternion & b)
 		: a(a), b(b)
 	{ }
 
-	
+	biquaternion (const dual_number & n0, const dual_number & n1, const dual_number & n2, const dual_number & n3)
+		: a (n0.real, n1.real, n2.real, n3.real),
+		  b (n0.imag, n1.imag, n2.imag, n3.imag)
+	{ }
+
+	biquaternion (const dual_number & scalar, const dual_vector & vector)
+		: a (scalar.real, vector.real),
+		  b (scalar.imag, vector.imag)
+	{ }
+
+
+	//! Компоненты бикватерниона - дуальные числа.
+	dual_number operator[] (int idx) const {
+		return dual_number (a[idx], b[idx]);
+	}
+
+	//! Скалярная часть бикватерниона - дуальное число, образованное скалярными частями кватернионов.
+	dual_number get_scalar() const {
+		return dual_number (a.get_scalar(), b.get_scalar());
+	}
+
+	//! Векторная часть бикватерниона - дуальный вектор, образованный векторными частями кватернионов.
+	dual_vector get_vector() const {
+		return dual_vector (a.get_vector(), b.get_vector());
+	}
+
+
 	biquaternion operator+ (const biquaternion & q) const {
 		return biquaternion (a+q.a, b+q.b);
 	}
